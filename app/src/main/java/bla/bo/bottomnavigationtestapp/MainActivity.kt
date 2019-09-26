574dd8db199db967b4f7e9442bb9a0d7fba9e34c
Yu Mitsuhori <ymbullseye@gmail.com>
2019-09-26 10:27:42 +0900

    fixed with reflection

--- app/src/main/java/bla/bo/bottomnavigationtestapp/MainActivity.kt
+++ app/src/main/java/bla/bo/bottomnavigationtestapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package bla.bo.bottomnavigationtestapp
 
 import android.os.Bundle
+import android.transition.AutoTransition
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import bla.bo.bottomnavigationtestapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
@@ -29,5 +31,16 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.bottomNav.setOnNavigationItemSelectedListener(this)
+        binding.bottomNav.fixBlinking()
+    }
+
+    private fun BottomNavigationView.fixBlinking() {
+        val menuView = getChildAt(0) as BottomNavigationMenuView
+        with(menuView::class.java.getDeclaredField("set")) {
+            isAccessible = true
+            set(menuView, androidx.transition.AutoTransition().apply {
+                duration = 0L
+            })
+        }
     }
 }
